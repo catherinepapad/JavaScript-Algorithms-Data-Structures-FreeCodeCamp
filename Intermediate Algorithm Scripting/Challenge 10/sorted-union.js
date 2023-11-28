cdf676d7852553a1e516e16397b9e30534e2f46e
catherinepapad <katerinapapado.01@gmail.com>
2023-11-28 14:32:11 +0200

    Add challenge 10

---
+++ Intermediate Algorithm Scripting/Challenge 10/sorted-union.js
@@ -0,0 +1,15 @@
+function uniteUnique(arr) {
+  let newArr = [];
+
+  for (let i=0 ; i<arguments.length ; i++) {
+    for (let j=0 ; j<arguments[i].length ; j++) {
+      if (newArr.indexOf(arguments[i][j]) === -1) {
+        newArr.push(arguments[i][j]);
+      }
+    }
+  } 
+
+  return newArr
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
